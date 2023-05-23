253ef049fef04269e805453d4decdafd8dafaf06
harry <harry.nguyen.2016@gmail.com>
2023-05-23 14:58:54 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Unit_8_Intro_To_Angular_Lab';
+}
